209d48a20bad02d8465350a766d707540e2d246a
metrnametr <patronius1515@gmail.com>
2020-05-26 13:24:35 +0300

    lab 7 queue done

---
+++ lab_7_queue/index.js
@@ -0,0 +1,50 @@
+/// JS Arrays has own methond push and pop, but i will realize custom method
+
+const Flower = require('../flowers');
+
+class Queue {
+    constructor(){
+        this._queue = new Array();
+    }
+
+    unshift(value){ //add element on queue
+        this._queue = [ value, ...this._queue];
+    }
+
+    shift(){ //delete element from queue
+        if (this.isEmpty()) {
+            console.log('Queue is empty');
+        } else {
+            this._queue = this._queue.slice(1, this._queue.length);
+        }
+    }
+
+    isEmpty(){
+        return !this._queue.length;
+    }
+
+    printQueue(){
+        this._queue.forEach(it => console.log(it));
+    }
+}
+
+const queue = new Queue();
+const count = 10;
+for (let i = 0; i < count; i++) {
+    const flower = new Flower('Pion', 'Nederland', 1.5, 20, 'pink');
+    queue.unshift(flower)
+}
+
+queue.printQueue();
+
+
+console.log(`Queue is empty ${queue.isEmpty() ? 'Yes' : 'No'}`);
+
+for (let i = 0; i < count; i++) {
+    queue.shift();
+}
+
+queue.printQueue();
+
+
+console.log(`Queue is empty ${queue.isEmpty() ? 'Yes' : 'No'}`);
